52033ef7fbbb1d187a9f103b01460a8d3762b4d1
Pablo <pavel.konstantinov1@mail.ru>
2023-10-22 20:42:27 +0800

    init project

---
+++ src/components/CompanyInfo/CompanyInfo.props.ts
@@ -0,0 +1,5 @@
+
+
+export interface CompanyInfoProps {
+    companyName: string, 
+}
---
+++ src/components/CompanyInfo/CompanyInfo.tsx
@@ -0,0 +1,13 @@
+import { CompanyInfoProps } from "./CompanyInfo.props";
+import styles from "./CompanyInfo.module.css"; 
+
+
+const CompanyInfo = ({}: CompanyInfoProps): JSX.Element => {
+    return (
+        <div className={styles.wrapper}>
+
+        </div>
+    )
+}
+
+export default CompanyInfo; 
